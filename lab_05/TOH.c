0bd634c6406f9a90cd8c26a30c3da15e47ffd257
samrat-dotel <samratdotel7264@gmail.com>
2024-01-31 08:36:39 +0545

    code for tower of hanoi in c

--- lab_05/TOH.c
+++ lab_05/TOH.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 
-int main(){
-    printf("Hello world");
+void tower(int n, char begin, char end, char aux)
+{
+    if (n == 1) 
+    {
+        printf("Move disk %d from rod %c to rod %c\n", n, begin, end);
+        return;
+    }
+    tower(n - 1, begin, aux, end);
+    printf("Move disk %d from rod %c to rod %c\n", n, begin, end);
+    tower(n - 1, aux, end, begin);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of disks:");
+    scanf("%d", &n);
+
+    tower(n, 'a', 'b', 'c');
+    return 0;
 }
